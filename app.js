b10d91bacc37278d7cf856c856b90fcfbfd56f41
Dren Morina <drenmorina321@gmail.com>
2024-09-11 00:33:35 +0200

    Create dialog box containing basic form

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
+const newBookBtn = document.querySelector(".new-book");
+const dialog = document.querySelector(".dialog");
+const submitBtn = document.querySelector(".submit-btn");
+
 const myLibrary = [];
+
+newBookBtn.addEventListener("click", () => {
+  dialog.show();
+});
+
+submitBtn.addEventListener("click", () => {
+  dialog.close();
+});
 
 function Book(title, author, pages, read) {
   this.title = title;
