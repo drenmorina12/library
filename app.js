867c072b6ebfab309ae7a050b8594dc832b3af16
Dren Morina <drenmorina321@gmail.com>
2024-09-11 01:56:49 +0200

    User submited books are added to book library

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const newBookBtn = document.querySelector(".new-book");
 const dialog = document.querySelector(".dialog");
 const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector("#form");
 
 const myLibrary = [];
 
@@ -8,7 +9,22 @@ newBookBtn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", () => {});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#title");
+  const author = document.querySelector("#author");
+  const pages = document.querySelector("#pages");
+  const status = document.querySelector('input[name="status"]:checked')
+    ? document.querySelector('input[name="status"]:checked')
+    : null;
+
+  let book = new Book(title.value, author.value, pages.value, status.value);
+
+  addBookToLibrary(book);
+  form.reset();
   dialog.close();
 });
 
@@ -19,6 +35,6 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
